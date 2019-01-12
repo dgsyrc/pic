58d6170b734d5d5d622af1904682c130bd2e6635
dgsyrc <33740858+dgsyrc@users.noreply.github.com>
2019-01-12 21:17:06 +0800

    Delete index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-$url[0] = "data/1.jpg";
-$url[1] = "data/2.jpg";
-$url[2] = "data/3.jpg";
-srand ((double)microtime()*1000000);
-$randomnum = rand(0, count($url)-1);
-header ("Location: $url[$randomnum]");
-?>
